09ab482578419f0c501ef003d9d7741b027f44ef
necromantiae <general1anaesthesia@gmail.com>
2023-03-28 19:46:06 +0200

    Master

--- js/script.js
+++ js/script.js
@@ -146,6 +146,32 @@ function americanBobtail() {fetch('https://api.thecatapi.com/v1/images/search?br
     'x-api-key': 'live_BLpAGAgFDsYnQrtHo3YFHy9HdB4h0yAzYOZCgDytYztFA0VpVqiRVNrowWOZBOXq'
   }
 }).then(res => res.json()).then(result => {
+    const image = document.querySelector('.miniature-1');
+    image.src = result[0].url;
+      image.classList.remove('hidden');
+      const loader = document.querySelector('.loader');
+      loader.classList.add('hidden');
+  })
+}
+
+function americanBobtailMini() {fetch('https://api.thecatapi.com/v1/images/search?breed_ids=abob', {
+  headers: {
+    'x-api-key': 'live_BLpAGAgFDsYnQrtHo3YFHy9HdB4h0yAzYOZCgDytYztFA0VpVqiRVNrowWOZBOXq'
+  }
+}).then(res => res.json()).then(result => {
+    const image = document.querySelector('.miniature-2');
+    image.src = result[0].url;
+      image.classList.remove('hidden');
+      const loader = document.querySelector('.loader');
+      loader.classList.add('hidden');
+  })
+}
+
+function americanBobtailMiniTwo() {fetch('https://api.thecatapi.com/v1/images/search?breed_ids=abob', {
+  headers: {
+    'x-api-key': 'live_BLpAGAgFDsYnQrtHo3YFHy9HdB4h0yAzYOZCgDytYztFA0VpVqiRVNrowWOZBOXq'
+  }
+}).then(res => res.json()).then(result => {
     const image = document.querySelector('#cats-image');
     image.src = result[0].url;
       image.classList.remove('hidden');
@@ -167,12 +193,25 @@ function americanCurl() {fetch('https://api.thecatapi.com/v1/images/search?breed
   })
 }
 
-function americanShorthair() {fetch('https://api.thecatapi.com/v1/images/search?breed_ids=asho', {
+function americanCurlMini() {fetch('https://api.thecatapi.com/v1/images/search?breed_ids=acur', {
   headers: {
     'x-api-key': 'live_BLpAGAgFDsYnQrtHo3YFHy9HdB4h0yAzYOZCgDytYztFA0VpVqiRVNrowWOZBOXq'
   }
 }).then(res => res.json()).then(result => {
-    const image = document.querySelector('#cats-image');
+    const image = document.querySelector('.miniature-1');
+    image.src = result[0].url;
+      image.classList.remove('hidden');
+      const loader = document.querySelector('.loader');
+      loader.classList.add('hidden');
+  })
+}
+
+function americanCurlMiniTwo() {fetch('https://api.thecatapi.com/v1/images/search?breed_ids=acur', {
+  headers: {
+    'x-api-key': 'live_BLpAGAgFDsYnQrtHo3YFHy9HdB4h0yAzYOZCgDytYztFA0VpVqiRVNrowWOZBOXq'
+  }
+}).then(res => res.json()).then(result => {
+    const image = document.querySelector('.miniature-2');
     image.src = result[0].url;
       image.classList.remove('hidden');
       const loader = document.querySelector('.loader');
@@ -192,6 +231,33 @@ function americanShorthair() {fetch('https://api.thecatapi.com/v1/images/search?
       loader.classList.add('hidden');
   })
 }
+
+function americanShorthairMini() {fetch('https://api.thecatapi.com/v1/images/search?breed_ids=asho', {
+  headers: {
+    'x-api-key': 'live_BLpAGAgFDsYnQrtHo3YFHy9HdB4h0yAzYOZCgDytYztFA0VpVqiRVNrowWOZBOXq'
+  }
+}).then(res => res.json()).then(result => {
+    const image = document.querySelector('.miniature-1');
+    image.src = result[0].url;
+      image.classList.remove('hidden');
+      const loader = document.querySelector('.loader');
+      loader.classList.add('hidden');
+  })
+}
+
+function americanShorthairMiniTwo() {fetch('https://api.thecatapi.com/v1/images/search?breed_ids=asho', {
+  headers: {
+    'x-api-key': 'live_BLpAGAgFDsYnQrtHo3YFHy9HdB4h0yAzYOZCgDytYztFA0VpVqiRVNrowWOZBOXq'
+  }
+}).then(res => res.json()).then(result => {
+    const image = document.querySelector('.miniature-2');
+    image.src = result[0].url;
+      image.classList.remove('hidden');
+      const loader = document.querySelector('.loader');
+      loader.classList.add('hidden');
+  })
+}
+
 
 function americanWirehair() {fetch('https://api.thecatapi.com/v1/images/search?breed_ids=awir', {
   headers: {
@@ -199,6 +265,32 @@ function americanWirehair() {fetch('https://api.thecatapi.com/v1/images/search?b
   }
 }).then(res => res.json()).then(result => {
     const image = document.querySelector('#cats-image');
+    image.src = result[0].url;
+      image.classList.remove('hidden');
+      const loader = document.querySelector('.loader');
+      loader.classList.add('hidden');
+  })
+}
+
+function americanWirehairMini() {fetch('https://api.thecatapi.com/v1/images/search?breed_ids=awir', {
+  headers: {
+    'x-api-key': 'live_BLpAGAgFDsYnQrtHo3YFHy9HdB4h0yAzYOZCgDytYztFA0VpVqiRVNrowWOZBOXq'
+  }
+}).then(res => res.json()).then(result => {
+    const image = document.querySelector('.miniature-1');
+    image.src = result[0].url;
+      image.classList.remove('hidden');
+      const loader = document.querySelector('.loader');
+      loader.classList.add('hidden');
+  })
+}
+
+function americanWirehairMiniTwo() {fetch('https://api.thecatapi.com/v1/images/search?breed_ids=awir', {
+  headers: {
+    'x-api-key': 'live_BLpAGAgFDsYnQrtHo3YFHy9HdB4h0yAzYOZCgDytYztFA0VpVqiRVNrowWOZBOXq'
+  }
+}).then(res => res.json()).then(result => {
+    const image = document.querySelector('.miniature-2');
     image.src = result[0].url;
       image.classList.remove('hidden');
       const loader = document.querySelector('.loader');
@@ -219,15 +311,53 @@ function arabianMau() {fetch('https://api.thecatapi.com/v1/images/search?breed_i
   })
 }
 
+function arabianMauMini() {fetch('https://api.thecatapi.com/v1/images/search?breed_ids=amau', {
+  headers: {
+    'x-api-key': 'live_BLpAGAgFDsYnQrtHo3YFHy9HdB4h0yAzYOZCgDytYztFA0VpVqiRVNrowWOZBOXq'
+  }
+}).then(res => res.json()).then(result => {
+    const image = document.querySelector('.miniature-1');
+    image.src = result[0].url;
+      image.classList.remove('hidden');
+      const loader = document.querySelector('.loader');
+      loader.classList.add('hidden');
+  })
+}
+
+function arabianMauMiniTwo() {fetch('https://api.thecatapi.com/v1/images/search?breed_ids=amau', {
+  headers: {
+    'x-api-key': 'live_BLpAGAgFDsYnQrtHo3YFHy9HdB4h0yAzYOZCgDytYztFA0VpVqiRVNrowWOZBOXq'
+  }
+}).then(res => res.json()).then(result => {
+    const image = document.querySelector('.miniature-2');
+    image.src = result[0].url;
+      image.classList.remove('hidden');
+      const loader = document.querySelector('.loader');
+      loader.classList.add('hidden');
+  })
+}
+
 abyssinianCat.addEventListener('click', abyssinian)
 aeganCat.addEventListener('click', aegan)
 button.addEventListener('click', getCats)
+button.addEventListener('click', getCatsMini)
+button.addEventListener('click', getCatsMiniTwo)
 showNav.addEventListener('click', removeHidden)
 americanBobtailCat.addEventListener('click', americanBobtail)
+americanBobtailCat.addEventListener('click', americanBobtailMini)
+americanBobtailCat.addEventListener('click', americanBobtailMiniTwo)
 americanCurlCat.addEventListener('click', americanCurl)
+americanCurlCat.addEventListener('click', americanCurlMini)
+americanCurlCat.addEventListener('click', americanCurlMiniTwo)
 americanShorthairCat.addEventListener('click', americanShorthair)
+americanShorthairCat.addEventListener('click', americanShorthairMini)
+americanShorthairCat.addEventListener('click', americanShorthairMiniTwo)
 americanWirehairCat.addEventListener('click', americanWirehair)
+americanWirehairCat.addEventListener('click', americanWirehairMini)
+americanWirehairCat.addEventListener('click', americanWirehairMiniTwo)
 arabianMauCat.addEventListener('click', arabianMau)
+arabianMauCat.addEventListener('click', arabianMauMini)
+arabianMauCat.addEventListener('click', arabianMauMiniTwo)
 abyssinianCat.addEventListener('click', abyssinianMini)
 abyssinianCat.addEventListener('click', abyssinianMiniTwo)
 aeganCat.addEventListener('click', aeganMini)
